782ba5c6dc72835d3603016cb73ae9de175e8f88
otkshol <charm.ot.trog@gmail.com>
2022-09-25 21:15:25 +0900

    Initial commit

---
+++ src/main/kotlin/com/example/youtubemovieanalysisapi/YoutubeMovieAnalysisApiApplication.kt
@@ -0,0 +1,11 @@
+package com.example.youtubemovieanalysisapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class YoutubeMovieAnalysisApiApplication
+
+fun main(args: Array<String>) {
+    runApplication<YoutubeMovieAnalysisApiApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/youtubemovieanalysisapi/YoutubeMovieAnalysisApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.youtubemovieanalysisapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class YoutubeMovieAnalysisApiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
